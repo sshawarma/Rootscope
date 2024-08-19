6185deb5886e07587c006e02ed99fd7be5a3c37f
Shubh Sharma <shubhsharma2022@gmail.com>
2024-08-18 18:14:44 -0700

    Log mqtt messages for testing

--- server.ts
+++ server.ts
@@ -4,6 +4,7 @@ import 'dotenv/config';
 
 import { EventPacket } from './src/providers/types/daemonEvent';
 import EventPacketHandler from './src/providers/packets/packetHandler';
+import { unpack } from 'msgpackr';
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -12,28 +13,31 @@ app.get('/', (req, res) => {
     res.send('Hello World! swag');
 });
 const client = mqtt.connect(process.env.MQTT_URL, {
-    clientId: 'ExampleClient1',
+    clientId: 'ExampleClient2',
     username: process.env.MQTT_USERNAME,
     password: process.env.MQTT_PASSWORD
 });
 
 client.on('connect', function () {
-    client.subscribe('rootscope-daemon1');
+    client.subscribe('rootscope-daemon2');
 });
 
 client.on('message', function (topic, message) {
-    const packetHandler: EventPacketHandler = EventPacketHandler.getInstance()
+    const packetHandler: EventPacketHandler = EventPacketHandler.getInstance();
     const mqttMessage: EventPacket = JSON.parse(message.toString());
-    console.log('unstring', message);
-    packetHandler.process(mqttMessage)
+    // packetHandler.process(mqttMessage)
+    console.log(message.toString())
+    // const y: Buffer = Buffer.from(mqttMessage.packed_data, 'base64');
 
-    console.log(
-        `Message received from topic: ${topic} \n
-         Message content as string: ${message.toString()}`
-    );
+    // console.log(unpack(y));
+
+    // console.log(
+    //     `Message received from topic: ${topic} \n
+    //      Message content as string: ${message.toString()}`
+    // );
 
     try {
-        console.log('unpackedMessage:');
+        // console.log('unpackedMessage:');
 
         client.publish('rootscope-daemon-ack', 'success');
     } catch (error) {
