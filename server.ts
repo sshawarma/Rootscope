b56849a24e8e5161330f00b7d1a09a9282a9f982
Shubh Sharma <shubhsharma2022@gmail.com>
2024-11-14 15:01:38 -0500

    Send error everytime for test

--- server.ts
+++ server.ts
@@ -5,6 +5,9 @@ import { MqttClient } from 'mqtt';
 import MQTTClientProvider from './src/lib/mqttClientProvider';
 import EventPacketHandler from './src/providers/packets/packetHandler';
 import { EventPacket } from './src/providers/types/daemonEvent';
+import { fsTestPacket } from './testfiles/packet';
+import { ErrorMessage, ErrorType } from './src/providers/types/errors';
+import { EventType } from './src/providers/types/fileSystemChangeEvent';
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -16,15 +19,23 @@ app.get('/', (req, res) => {
 const mqttClient: MqttClient = MQTTClientProvider.getClient();
 
 mqttClient.on('connect', function () {
-    mqttClient.subscribe({ 'rootscope-daemon2': { qos: 1 } });
+    mqttClient.subscribe({ 'rootscope-daemon4': { qos: 1 } });
+    console.log('connected')
 });
 
-mqttClient.on('message', function (topic, message) {
-    const packetHandler: EventPacketHandler = EventPacketHandler.getInstance();
+mqttClient.on('message', async (topic, message) => {
+    // const packetHandler: EventPacketHandler = EventPacketHandler.getInstance();
     const mqttMessage: EventPacket = JSON.parse(message.toString());
-    packetHandler.process(mqttMessage);
+                const messageToPublish: ErrorMessage = {
+                errorType: ErrorType.OUT_OF_ORDER,
+                eventType: mqttMessage?.event_type ?? EventType.UNKNOWN,
+                timeOfError: new Date(),
+                data: { eventId: 4124 }
+            };
+            MQTTClientProvider.publishToTopic(JSON.stringify(messageToPublish));
+    // await packetHandler.process(mqttMessage);
 });
 
-app.listen(port, () => {
+app.listen(port, async () => {
     return console.log(`Express is listening at http://localhost:${port}`);
 });
